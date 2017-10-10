f5a6cb949f1918105fce0f96dfe03f5a4280803b
wwmairs <wwmairs@gmail.com>
2017-10-10 16:02:51 -0400

    two sample data points

---
+++ test-data.js
@@ -0,0 +1,65 @@
+var point1 = {
+    _id: null,
+    title: "Somerville House Cat Complete Moon Mission",
+    author: "Dennis the Cat",
+    url: "hmm.com/story",
+    header_photo_url: "https://i.ytimg.com/vi/E7BnKFl7lYI/hqdefault.jpg",
+    published_date: new Date(),
+    blurb: "Cat-stronaut pilots dry-food fueled engine to dark side of moon",
+    tags: ["somerville", "cats", "news"],
+    location: {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -71.12341225147247,
+              42.402303114395295
+            ],
+            [
+              -71.12352490425108,
+              42.40216050967468
+            ],
+            [
+              -71.12340152263641,
+              42.402081284689814
+            ],
+            [
+              -71.12329423427582,
+              42.40226350200542
+            ],
+            [
+              -71.12341225147247,
+              42.402303114395295
+            ]
+          ]
+        ]
+      }
+    },
+    location_name: "28 Whitman St"
+}
+
+var point2 = {
+    _id: null,
+    title: "Tufts Students Discover Gold in Sewer",
+    author: "Tricky Snarkaretos",
+    url: "this.isnt.real.co.uk",
+    header_photo_url: "http://1.bp.blogspot.com/-5ZbU7ui6v_Q/Ti26NCiFeuI/AAAAAAAAHxw/B2HBln5KQcg/s1600/fW5nZ.St.11.jpg",
+    published_date: Date(),
+    blurb: "Three Tufts students stuck it rich, discovering hidden gold in a storm drain while looking for that old iPhone 6 they dropped on the way to spanish class",
+    tags: ["somerville", "tufts", "sewer"],
+    location: {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -71.12189412117003,
+          42.406751426673765
+        ]
+      }
+    },
+    location_name: "That one storm drain by Monaco's house"
+}
