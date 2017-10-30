646a16d79311e05ecca512727689de7e1c77abaa
Alex Johnson <alex.johnson969@gmail.com>
2017-10-29 21:55:44 -0400

    fixed incorrect login redirect

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -35,7 +35,7 @@ router.post('/register', function(req, res) {
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login');
+    res.redirect('/account/login');
 }
 
 router.get('/server', ensureAuthenticated, function(req, res) {
