df5785ac08f4894d33bb68d83f42c0f8736c8ffd
Adam Kercheval <arkercheval@gmail.com>
2017-10-31 14:07:44 -0400

    update POST infrastructure and admin.html layout

--- public/admin.js
+++ public/admin.js
@@ -1,19 +1,17 @@
 var startLat = 42.3601;
 var startLng = -71.0589;
 
-var map;
-var geocoder;
-var user;
+var map, geocoder, user;
+
 var coordinates = [];
 
-var button;
+var hostname = location.hostname;
+var port = location.port;
 
-
+initMap();
 
 function initMap()
 {
-
-    console.log("initMap called");
     map = new google.maps.Map(document.getElementById('map_canvas'), {
         zoom: 12,
         center: {lat: startLat, lng: startLng}
@@ -28,48 +26,23 @@ function initMap()
         geocoder.geocode({'latLng': latLng}, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 if (results[1]) {
-                    document.getElementById("location").value = results[1].formatted_address;
+                    document.getElementById("location_name").value = results[1].formatted_address;
                 }
             }
         })
     }
 }
 
-document.onload = google.maps.event.addListener(map, "click", function (e) {
-    console.log("event triggered\n");
-    alert("recognized click")
-    //lat and lng is available in e object
-    //var latLng = e.latLng;
-    //console.log(latlng);
-});
-
 $(document).ready(function() {
-$("#submitbutton").on('click', function(){
-    alert("submit started");
-    var dataObject = new Object();
-    dataObject.title = $('#title').val();
-    dataObject.author = $('#author').val();
-    dataObject.url = $('#url').val();
-    dataObject.header_photo_url = $('#header_photo_url').val();
-    dataObject.published_date = new Date();
-    dataObject.blurb = $('#blurb').val();
-    dataObject.location_name = $('#location_name').val();
-    dataObject.coordinates = this.coordinates;
-
-    button = story;
-    console.log($('#title').val());
-
-    var dataString = JSON.stringify(dataObject);
-
-    $.ajax({
-        url: "http://" + this.hostname + ":" + this.port + "/stories/",
-        type: 'POST',
-        data: {
-            story: dataString
-        },
-        success: function(response) {
-            alert ("success");
-        }
-    })
-})
-})
+    $("#submitbutton").on('click', function(){
+        $.post("http://" + location.hostname + ":" + location.port + "/stories", {
+            "title": $('#title').val(),
+            "author": $('#author').val(),
+            "url": $('#url').val(),
+            "header_photo_url": $('#header_photo_url').val(),
+            "published_date": new Date(),
+            "blurb": $('#blurb').val(),
+            "location_name": $('#location_name').val()
+        })
+    });
+});
