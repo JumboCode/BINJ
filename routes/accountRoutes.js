72a694ad5447a3f19ca7ef8128ed874995b67906
Alex Johnson <alex.johnson969@gmail.com>
2017-11-02 15:51:50 -0400

    cleanup

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -4,15 +4,17 @@ const path = require('path');
 var Account = require('../models/accountModel');
 var router = express.Router();
 
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/account/login');
+}
+
 /* temporary solution for testing : */
 router.get('/login', function(req, res) {
     res.sendFile('login.html', {root: path.join(__dirname, '../public')});
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-	console.log("logging in")
-    //res.redirect('/');
-    console.log(req.user);
     res.send("logged on!")
 });
 
@@ -26,17 +28,11 @@ router.post('/register', function(req, res) {
         if (err) {
             return res.sendStatus(500);
         }
-
         passport.authenticate('local')(req, res, function () {
             res.redirect('/');
         });
     });
 });
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/account/login');
-}
 
 router.get('/server', ensureAuthenticated, function(req, res) {
     res.send("On");
