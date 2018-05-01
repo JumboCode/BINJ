93260caf60c34035a526ffe404137c38ef7f8b68
Adam Kercheval <arkercheval@gmail.com>
2018-05-01 12:58:15 -0400

    keyboard cat uncomment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(session({ secret: 'keyboard cat' }));
+app.use(session({ secret: 'keyboard cat' }));
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
